da60e4aabbe49cd861d16de692c1e9c6446b7fe5
Yuta Sawamura <>
2021-12-24 19:28:01 +0900

    add_email_custom_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
+require "validator/email_validator"
+
 class User < ApplicationRecord
+  before_validation :downcase_email
+
   has_secure_password
 
   validates :name, presence: true,
@@ -6,6 +10,8 @@ class User < ApplicationRecord
       maximum: 30,
       allow_blank: true
     }
+  validates :email, presence: true,
+    email: { allow_blank: true }
 
   VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
   validates :password, presence: true,
@@ -19,4 +25,24 @@ class User < ApplicationRecord
       allow_blank: true
     },
     allow_nil: true
+
+  class << self
+    # emailからアクティブなユーザーを返す
+    def find_by_activated(email)
+      find_by(email: email, activated: true)
+    end
+  end
+
+  # 自分以外の同じemailのアクティブなユーザーがいる場合にtrueを返す
+  def email_activated?
+    users = User.where.not(id: id)
+    users.find_by_activated(email).present?
+  end
+
+  private
+
+    # email小文字化
+    def downcase_email
+      self.email.downcase! if email
+    end
 end
