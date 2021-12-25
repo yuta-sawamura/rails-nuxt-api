53fa8b31697919b3f3e576a3819d9a1c274bfc4e
Yuta Sawamura <>
2021-12-25 09:06:21 +0900

    test_environment_setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,15 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
+
+  parallelize_setup do |worker|
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
